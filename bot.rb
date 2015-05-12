aa92e5e5fd4bdeff6ae1952364da41df6247f2e7
Kiran Joy <kiranjoysebastian@gmail.com>
2015-05-12 16:30:53 +0800

    finished a skeleton for the rest of the bot

--- bot.rb
+++ bot.rb
@@ -1,6 +1,9 @@
 require "twitter"
+# Modify config_sample.rb with proper variables
 require "./config.rb"
 
+# Initialize clients
+# Streaming client to monitor incoming streams
 client = Twitter::Streaming::Client.new do |config|
   config.consumer_key        = Settings.CONSUMER_KEY
   config.consumer_secret     = Settings.CONSUMER_SECRET
@@ -8,6 +11,7 @@ client = Twitter::Streaming::Client.new do |config|
   config.access_token_secret = Settings.ACCESS_SECRET
 end
 
+# Rest client to update status using rest api
 rest_client = Twitter::REST::Client.new do |config|
   config.consumer_key        = Settings.CONSUMER_KEY
   config.consumer_secret     = Settings.CONSUMER_SECRET
@@ -15,15 +19,55 @@ rest_client = Twitter::REST::Client.new do |config|
   config.access_token_secret = Settings.ACCESS_SECRET
 end
 
-rest_client.update("@_kjsebastian hello", in_reply_to_status_id: "598027760435474432")
+# Parse the tweet uri (Addressable::URI) to get the user and tweet id
+def parse_tweet_uri(uri)
+  uri_elements = uri.path.split("/")
+  user_screen_name = uri_elements[1]
+  tweet_id = uri_elements.last
+  puts user_screen_name, tweet_id
 
+  return user_screen_name, tweet_id
+end
+
+def check_tweet(text)
+  return text.include? "define"
+end
+
+#TODO
+def get_word_to_define(text)
+  return
+end
+
+#TODO
+def urban_define(word)
+  return
+end
+
+# Process the tweet object retrieving necessary information
+# who to reply to
+# which tweet id to reply to
+# check for 'define' keyword
+# if keyword exists get info from urbandict api
+# reply to tweet
+def process(tweet)
+  reply_to_user, reply_to_tweet = parse_tweet_uri(tweet.uri)
+  has_define = check_tweet(tweet.text)
+  if has_define
+    word = get_word_to_define(tweet.text)
+    definition = urban_define(word)
+    reply_tweet_text = "@#{reply_to_user}, #{word} means #{definition}"
+    rest_client.update(reply_tweet_text, in_reply_to_status_id: reply_to_tweet)
+  else
+    reply_tweet_text = "@#{reply_to_user}, I'm just a bot doing bot things. Try '@urban_bot define trollface'"
+    rest_client.update(reply_tweet_text, in_reply_to_status_id: reply_to_tweet)
+  end
+end
+
+# Start listening for '@' mentions
 client.user do |object|
   case object
   when Twitter::Tweet
-    puts "Tweet"
-    puts object.in_reply_to_screen_name
-    puts object.in_reply_to_user_id
-    puts object.user_mentions[0].name
-    puts object.uri
+    process(object)
   end
 end
+
