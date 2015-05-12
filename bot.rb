2edfdd79fa244f82e24d7c6aac87738e049adaad
Kiran Joy <kiranjoysebastian@gmail.com>
2015-05-12 14:45:52 +0800

    twitter gem working

--- bot.rb
+++ bot.rb
@@ -1,2 +1,11 @@
 require "twitter"
+require "./config.rb"
 
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = Settings.CONSUMER_KEY
+  config.consumer_secret     = Settings.CONSUMER_SECRET
+  config.access_token        = Settings.ACCESS_TOKEN
+  config.access_token_secret = Settings.ACCESS_SECRET
+end
+
+client.update("tweet from ruby!")
