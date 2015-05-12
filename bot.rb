099d4a356c10c7ad31f53ef037d905dc35b90302
Kiran Joy <kiranjoysebastian@gmail.com>
2015-05-12 14:53:35 +0800

    try the streaming api

--- bot.rb
+++ bot.rb
@@ -1,11 +1,13 @@
 require "twitter"
 require "./config.rb"
 
-client = Twitter::REST::Client.new do |config|
+client = Twitter::Streaming::Client.new do |config|
   config.consumer_key        = Settings.CONSUMER_KEY
   config.consumer_secret     = Settings.CONSUMER_SECRET
   config.access_token        = Settings.ACCESS_TOKEN
   config.access_token_secret = Settings.ACCESS_SECRET
 end
 
-client.update("tweet from ruby!")
+client.sample do |object|
+  puts object.text if object.is_a?(Twitter::Tweet)
+end
