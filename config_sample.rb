1acc03c57350a2c4cf5e9788ccd407f19114af05
Kiran Joy <kiranjoysebastian@gmail.com>
2015-05-12 14:39:56 +0800

    sample configuration for api keys

---
+++ config_sample.rb
@@ -0,0 +1,22 @@
+module Config
+    @CONSUMER_KEY = ""
+    @CONSUMER_SECRET = ""
+    @ACCESS_TOKEN = ""
+    @ACCESS_SECRET = ""
+
+    def self.CONSUMER_KEY
+        return @CONSUMER_KEY
+    end
+
+    def self.CONSUMER_SECRET
+        return @CONSUMER_SECRET
+    end
+
+    def self.ACCESS_TOKEN
+        return @ACCESS_TOKEN
+    end
+
+    def self.ACCESS_SECRET
+        return @ACCESS_SECRET
+    end
+end
